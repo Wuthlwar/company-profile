4f579cb3a6e2b43007bee714df71f40cf6adde1f
WutYeePhyoPro1 <wutyeephyo.pro1@gmail.com>
2023-02-07 09:58:56 +0000

    udpate

--- resources/views/admins/app.blade.php
+++ resources/views/admins/app.blade.php
@@ -296,13 +296,13 @@
       </footer>
     </div>
     <!-- CoreUI and necessary plugins-->
-    <script src="vendors/@coreui/coreui/js/coreui.bundle.min.js"></script>
-    <script src="vendors/simplebar/js/simplebar.min.js"></script>
+    <script src="{{ asset('admin/dist/vendors/@coreui/coreui/js/coreui.bundle.min.js') }}"></script>
+    <script src="{{ asset('admin/dist/vendors/simplebar/js/simplebar.min.js') }}"></script>
     <!-- Plugins and scripts required by this view-->
-    <script src="vendors/chart.js/js/chart.min.js"></script>
-    <script src="vendors/@coreui/chartjs/js/coreui-chartjs.js"></script>
-    <script src="vendors/@coreui/utils/js/coreui-utils.js"></script>
-    <script src="js/main.js"></script>
+    <script src="{{ asset('admin/dist/vendors/chart.js/js/chart.min.js') }}"></script>
+    <script src="{{ asset('admin/dist/vendors/@coreui/chartjs/js/coreui-chartjs.js') }}"></script>
+    <script src="{{ asset('admin/dist/vendors/@coreui/utils/js/coreui-utils.js') }}"></script>
+    <script src="{{ asset('admin/dist/js/main.js') }}"></script>
     <script>
     </script>
 
