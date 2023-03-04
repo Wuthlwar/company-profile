9fb102ba249bf817e991d27e95629a3d15cf3bde
Whiro182 <105338042+Whiro182@users.noreply.github.com>
2023-03-04 13:00:18 +0630

    company-profile

---
+++ resources/lang/en/joint.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'joint_detail1'              =>'PRO1 GLOBAL HOME CENTER has 10 years experience of home building and living store center, which offers construction material such as structure and cement, roofing and ceiling, surface covering, tools and hardware products, home improvement products such as home appliance and houseware products, furniture, sanitaryware, electrical and lighting products, gardening products like pipe and garden accessories, and stationery and digital equipments in modern trade warehouse style. Currently, PRO 1 Global Home Center has totally 12 branches and trying to expansion branches to cover all Myanmar region and become the leader of one stop service home living product center.',
+    'joint_detail2'              =>'SCG, a leading business conglomerate in the ASEAN region, has committed itself to conducting business in line with good corporate governance and sustainable development principles throughout 100 years. The Group longstanding tradition of learning, adjustment and development in all areas has enabled SCG to survive the wave of crises and challenges and earn widespread recognition as a role model for other businesses, both locally and internationally.',
+    'joint_detail3'              =>'GBH, has an experience of home building & living store center 20 years, which offers construction material, home decorative products, tools and equipment used in construction, home improvement, and household and gardening decoration in modern trade warehouse style. In addition, GBH have the biggest distribution center along with automatic system to make an operation excellence and customer feel convenience.',
+];
