a38950ba4f2eb79d9b8712cc6e23111f3a1e039e
WutYeePhyoPro1 <wutyeephyo.pro1@gmail.com>
2024-06-11 10:55:59 +0800

    update

--- resources/lang/en/productreturn.php
+++ resources/lang/en/productreturn.php
@@ -11,7 +11,7 @@ return [
     'pro_return_info6'=> '6 .When return the ceramic tiles, we accept only the packing box quantity will be accepted.',
     'pro_return_info7'=> '7. The Items with Serial No: will be returned only with a Warranty Card issued by the manufacturer.',
     'pro_return_info8'=> '8. If you want to return the product caused by the defect of the manufacturing plant, you must send it directly to the manufacturing plant with the warranty card.',
-    'pro_return_info9'=> '9. We will only returns after 5% deduction from original amount.',
+    'pro_return_info9'=> '9. Please note that we reserve the right to amend these terms and conditions in the future without prior notice.',
     'pro_return_look' => 'Take a look at the Super Services of PRO 1 Global Home Center',
 
 ];
