f67164726a4b00d72107db599bf28583aa4fb177
unknown <Nyi Nyi Myat>
2024-09-03 13:21:41 +0630

    Update multi-lan 3924

--- resources/views/Faq.blade.php
+++ resources/views/Faq.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.main')
+
 @section('content')
     <div class="stricky-header stricked-menu main-menu">
         <div class="sticky-header__content"></div>
         <!-- /.sticky-header__content -->
     </div>
     <!-- /.stricky-header -->
+
     <section class="faq-one">
         <div class="container">
             <div class="row">
@@ -14,57 +16,27 @@
                 </div>
 
                 @foreach($faqs as $faq)
-                @php
-                $localetitle = session()->get('locale');
-                @endphp
+                    @php
+                        $locale = session()->get('locale', 'en'); // Default to 'en' if locale is not set
+                    @endphp
 
-                @if($localetitle=='en')
                     <div class="col-xl-12 col-lg-12">
                         <div class="faq_title">
-                            <h5>{{ $faq->title_en }}</h5>
+                            <h5>
+                                {{ $locale === 'mm' ? $faq->title_my : $faq->title_en }}
+                            </h5>
                         </div>
                         <br>
-                        <p>{!! $faq->content_en !!}</p>
+                        <p>
+                            {!! $locale === 'mm' ? $faq->content_my : $faq->content_en !!}
+                        </p>
                         <br>
                     </div>
-                    @elseif ($localetitle=='mm')
-                    <div class="col-xl-12 col-lg-12">
-                        <div class="faq_title">
-                            <h5>{{ $faq->title_my }}</h5>
-                        </div>
-                        <br>
-                        <p>{!! $faq->content_my !!}</p>
-                        <br>
-                    </div>
-                @endif
-
-
 
                     @if($faq->faqQans)
                         @foreach($faq->faqQans as $faqQans)
-                            @php
-                                $locale = session()->get('locale');
-                            @endphp
-                            {{-- @dd($locale); --}}
-                            @if($locale=='en' && $faqQans->language == 'en')
-                                <div class="col-xl-12 col-lg-12">
-                                    <div class="faq-one__single">
-                                        <div class="accrodion-grp faq-one-accrodion" data-grp-name="faq-one-accrodion-1">
-                                            <div class="accrodion">
-                                                <div class="accrodion-title">
-                                                    <h4><span>?</span>{{ $faqQans->question }}</h4>
-                                                </div>
-                                                <div class="accrodion-content">
-                                                    <div class="inner">
-                                                        {!! $faqQans->answer !!}
-                                                    </div>
-                                                    <!-- /.inner -->
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            @elseif($locale=='mm' && $faqQans->language == 'my')
+                            @if(($locale === 'en' && $faqQans->language === 'en') ||
+                               ($locale === 'mm' && $faqQans->language === 'my'))
                                 <div class="col-xl-12 col-lg-12">
                                     <div class="faq-one__single">
                                         <div class="accrodion-grp faq-one-accrodion" data-grp-name="faq-one-accrodion-1">
